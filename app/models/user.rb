906c5e1a31d45cc529213b1dbd8153d474c84f7e
Zohaib <zohaibk1111@gmail.com>
2020-08-12 23:25:35 -0400

    validation for user model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :password, length: { minimum: 8 } ( message: "Password must be above 8 characters long")
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    
 end
