cee0fee9055377abbb4ddb017e2ddc491cb28319
Zohaib <zohaibk1111@gmail.com>
2020-08-13 00:44:21 -0400

    added jwt token into the user_controller file... Aslo used .except to prevent the password_digest from being sent to the front end..should only stay in the back end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if @user.save  #saves data input 
+      @token = encode({id: @user.id})
+      render json: {
+        @user.attributes.except("password_digest"),
+        token: @token
+      }, status :created
+  
     else
       render json: @user.errors, status: :unprocessable_entity
     end
