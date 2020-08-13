e8c12f9c8fd060b509c5cc4a2f733eb3e6c37f60
Zohaib <zohaibk1111@gmail.com>
2020-08-13 18:05:28 -0400

    Data seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Modification.destroy_all
+Vehicle.destroy_all
+User.destroy_all
 
-@admin = User.create(username: 'admin', email: 'zohaib@admin.com', password: '12345678')
 
-first_car = Vehicle.create(vehicle_type: 'car', make: "Ford", model: 'Mustang', year: '2019', 
+@admin = User.create!(username: 'admin', email: 'zohaib@admin.com', password: '12345678')
+puts "#{User.count} users created"
+
+@first_car = Vehicle.create!(vehicle_type: 'car', make: "Ford", model: 'Mustang', year: '2019', 
     engine_type: '4 cylinder ecoboost', 
     description: "My first car. It is my daily driver and project car",
     color: 'red',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6tNGoYgUUaCH8HOGcitApMYeHr-VMbTmY5g&usqp=CAU',
-    user_id: @admin
+    user: @admin
 )
+puts "#{Vehicle.count} vehicles created"
 
-@cold_air_intake = Modification.create(
+
+@cold_air_intake = Modification.create!(
     name: 'cold air intake',
     brand_name: 'cobb',
     mod_type: true,
     performance_gain: 25,
     cost: 500,
-    vehicle_id: @first_car
-
+    vehicle: @first_car
 )
+
+puts "#{Modification.count} modifications created"
+
+@first_car.modifications.push(@cold_air_intake)
