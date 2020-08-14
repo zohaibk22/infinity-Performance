a43ba4ad31fde6156521baf83e8b9a7f0da90cca
Zohaib <zohaibk1111@gmail.com>
2020-08-14 15:47:35 -0400

    Altered package.json file

--- client/src/Services/api-helper.js
+++ client/src/Services/api-helper.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? /* link to your heroku app. Example:*/ "https://infinity-auto-api.herokuapp.com/"
+    : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
