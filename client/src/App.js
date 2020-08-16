ac2080efbfd49c676d98ab959fe896b2f610f693
Zohaib <zohaibk1111@gmail.com>
2020-08-16 01:04:50 -0400

    Verify User is fully functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/shared/Header/Header";
 import Main from "./Components/shared/Main/Main";
 import { Route, Link } from "react-router-dom";
+import { verifyUser } from "./Services/auth";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    handleVerify(); //checking to see whenever the page loads, if we have a auth token so that we can stay logged in
+  }, []); //reason why this is called in useEffect because useEffect runs whenever the component is mounted or rerender
+
+  const handleVerify = async () => {
+    const userData = await verifyUser(); //resetting the data we got back from our auth token, which is all the user credentials, back into setUserData
+    setCurrentUser(userData);
+  };
   return (
     <>
       <Header currentUser={currentUser} />
