01990eacc15469a951df22535c33be5d8554b6b5
Zohaib <zohaibk1111@gmail.com>
2020-08-19 01:48:32 -0400

    Sign in page done

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import {loginUser} from "../../Services/auth"
+import "./Login.css"
 
 export default function Login(props) {
     const [formData, setFormData] = useState({
@@ -24,28 +25,31 @@ export default function Login(props) {
     }
 
     return (
-        <div>
-            <h3>Login</h3>
-            <form onSubmit={handleSubmit}>
-                <label>Username:</label>
+        <div className='main-login-div'>
+            <h3 className = 'signin-title'>Sign In</h3>
+            <form onSubmit={handleSubmit} className = 'login-form'>
+                <label className='username-login'>Username:</label>
                 <input 
+                    className = "username-input"
                     type="text" 
                     name='username' 
                     value={formData.username} 
                     onChange = {handleChange}
                 /> 
 
-                <label>Password:</label> 
+                <label className='password-login'>Password:</label> 
                 <input 
+                     className = "password-input"
                     type="password" 
                     name="password" 
                     value = {formData.password} 
                     onChange = {handleChange}
                 />
 
-                <button>Submit</button> 
+                <button className='login-bttn bttn-combo'>Submit</button> 
+                <button className = 'register-bttn bttn-combo' onClick={()=>{props.history.push('/register')}}>Register</button>
 
-                <Link to='/register'>Register for account </Link>
+                {/* <Link to='/register'>Register for account </Link> */}
 
                 
             </form>
