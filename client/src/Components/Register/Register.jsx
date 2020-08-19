2f677306f20c060b16b42cb12648ba862e66458d
Zohaib <zohaibk1111@gmail.com>
2020-08-19 09:00:53 -0400

    media query done for mobile

--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -28,7 +28,7 @@ export default function Login(props) {
 
     return (
         <div className ='main-login-div'>
-            <h3 className ='signin-title'>Register</h3>
+            <h3 className ='register-title'>Register</h3>
             <form className='register-form 'onSubmit={handleSubmit}>
                 <label className="email-label-register">Email:</label>
                 <input 
